5ba1b8ee4f8800416148aa240fb2e6c7c785d251
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-08-26 09:31:07 +0200

    Lesson 66

---
+++ Lesson66.java
@@ -0,0 +1,18 @@
+
+public static void mian(String[] args) {
+	int count = 0;
+	int sum = 0;
+
+	for (int i = 1; i <= 1000; i++) {
+		if ((i % 3 == 0) && (i % 5 == 0)) {
+			count++;
+			sum += i;
+			System.out.println("Found number = " + i)
+		}
+		
+		if (count == 5) {
+			break;
+		}
+		
+		System.out.println("Sum = " + sum);
+}
