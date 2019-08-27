74b5064eab2840374dc6c214c482794656b9f804
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-08-27 08:43:02 +0200

    Add files via upload

--- Lesson67isEvenNumber.java
+++ Lesson67isEvenNumber.java
@@ -3,16 +3,36 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
+//        int number = 4;
+//        int finishNumber = 20;
+//
+//        while (number <= finishNumber) {
+//            number++;
+//            if(!isEvenNumber(number)) {
+//                continue;
+//            }
+//            System.out.println("Even number " + number);
+//        }
+
         int number = 4;
         int finishNumber = 20;
+        int evenNumbersFound = 0;
 
         while (number <= finishNumber) {
             number++;
             if(!isEvenNumber(number)) {
                 continue;
             }
+
             System.out.println("Even number " + number);
+
+            evenNumbersFound++;
+            if (evenNumbersFound >= 5) {
+                break;
+            }
         }
+
+        System.out.println("Total even number found = " + evenNumbersFound);
 	// write your code here
     }
 
