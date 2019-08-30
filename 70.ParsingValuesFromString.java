1aab41e30de99efebd39b1743604300e90f2cf1c
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-08-30 05:55:14 +0200

    Add files via upload

---
+++ 70.ParsingValuesFromString.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018.125";
+        System.out.println("numberAsString = " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+	// write your code here
+    }
+}
