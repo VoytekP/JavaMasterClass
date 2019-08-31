d738e6a047c2f3d8ed7692169495e3b1728af163
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-08-31 04:52:49 +0200

    Add files via upload

---
+++ 72.ReadingUserInputProbSol.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+
+            System.out.println("Enter yur name: ");
+            String name = scanner.nextLine();
+            int age = 2019 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
+        scanner.close();
+	// write your code here
+    }
+}
