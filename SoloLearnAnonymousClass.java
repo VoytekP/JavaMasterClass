db18cd0f4b46201e1d17090f1c1545d215a28493
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-09-15 21:21:01 +0200

    Add files via upload

---
+++ SoloLearnAnonymousClass.java
@@ -0,0 +1,14 @@
+Class Machine {
+public void start () {
+	System.out.println("Starting...");
+	}
+}
+
+class Program {
+public static void main(String[] args) {
+	Machine m = new Machine() {
+		@Override public("Woooo");
+		}
+	};
+	m.start();
+}
