abde72e36008ed83fcaee8cda34a5a8b30933a2d
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-09-18 09:09:26 +0200

    Add files via upload

---
+++ Excercise36.Cuboid.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height;
+        if (height < 0){
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+}
