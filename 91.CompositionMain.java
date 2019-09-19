1c9e0f836bc302da922f027ad330ea77539af549
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-09-19 08:37:35 +0200

    Add files via upload

--- 91.CompositionMain.java
+++ 91.CompositionMain.java
@@ -12,5 +12,25 @@ public class Main {
 
         PC thePC = new PC(theCase, theMonitor, theMotherboard);
         thePC.powerUp();
+
+        // Chalange
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12,55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Greg's", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        bedroom.makeBed();
+
+        bedroom.getLamp().turnOn();
+
+
     }
 }
