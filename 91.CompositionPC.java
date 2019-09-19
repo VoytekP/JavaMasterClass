d18d8ecccea7f79d833a07de8bcbdfc5c644fef7
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-09-19 06:50:51 +0200

    Add files via upload

---
+++ 91.CompositionPC.java
@@ -0,0 +1,23 @@
+package academy.learnprogramming;
+
+public class PC {
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy graphics to show
+        monitor.drawPixelAt(1200,50, "yellow");
+    }
+}
