d9c52226dc32a81779ad80c00f5be3be56a44e35
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-09-21 08:47:09 +0200

    Add files via upload

---
+++ 93.Lesson.EncapsulationChallangeMain.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(50, false);
+        System.out.println("initial page count = " +printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(5);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+    }
+}
