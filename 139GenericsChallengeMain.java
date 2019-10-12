ff5d9eec90ecfdd55195f01e3dcf9d37fef5521c
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-10-12 10:14:37 +0200

    Add files via upload

---
+++ 139GenericsChallengeMain.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
+
+//        footballLeague.add(baseballTeam);
+
+        footballLeague.showLeagueTable();
+    }
+}
