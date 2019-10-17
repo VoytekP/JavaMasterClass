a440bc1967938205016389db251fc0f8ec59b3af
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-10-17 05:54:34 +0200

    Add files via upload

---
+++ 145ScopeMain.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String privateVar = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance private Var is " + scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
+
+        scopeInstance.timesTwo();
+        System.out.println("*******************");
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+    }
+}
