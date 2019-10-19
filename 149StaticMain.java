3b4b0cb0482b40e3be3183ab0edb52422bb5bd89
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-10-19 08:54:16 +0200

    Add files via upload

---
+++ 149StaticMain.java
@@ -0,0 +1,28 @@
+package com.timbuchalka._static;
+
+public class Main {
+
+    public static int multiplier = 7;
+
+    public static void main(String[] args) {
+
+//        StaticTest firstInstance = new StaticTest(("1st Instance"));
+//        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest secondInstance = new StaticTest(("2nd Instance"));
+//        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest thirdInstance = new StaticTest(("3rd Instance"));
+//        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        // java com.company._static.Main
+        int answer = multiply(6);
+        System.out.println("The answer is " + answer);
+        System.out.println("Multiplier is " + multiplier);
+
+    }
+
+    public static int multiply(int number) {
+        return number * multiplier;
+    }
+}
