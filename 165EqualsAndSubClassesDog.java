650c9a4922005c1aaeef20ff7146ac026216d2c0
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-10-29 09:02:30 +0100

    Add files via upload

---
+++ 165EqualsAndSubClassesDog.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+public class Dog {
+    private final String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj instanceof Dog) {
+            String objName = ((Dog) obj).getName();
+            return this.name.equals(objName);
+        }
+
+        return false;
+    }
+}
