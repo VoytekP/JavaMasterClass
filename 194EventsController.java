612c110ae4fce43cc8badb390eb1c58d56a12301
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-11-08 06:07:41 +0100

    Add files via upload

--- 194EventsController.java
+++ 194EventsController.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -15,6 +17,8 @@ public class Controller {
     private Button byeButton;
     @FXML
     private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML
     public void initialize() {
@@ -30,12 +34,30 @@ public class Controller {
             System.out.println("Bye, " + nameField.getText());
         }
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException event) {
-            // we don't care about this
-        }
-        if (ourCheckBox.isSelected()) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                    System.out.println("I'm going to sleep on the: " + s);
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                            System.out.println("I'm updating the label on the: " + s);
+                            ourLabel.setText("We did something!");
+                        }
+                    });
+                } catch (InterruptedException event) {
+                    // we don't care about this
+                }
+            }
+        };
+
+        new Thread(task).start();
+
+       if (ourCheckBox.isSelected()) {
             nameField.clear();
             helloButton.setDisable(true);
             byeButton.setDisable(true);
