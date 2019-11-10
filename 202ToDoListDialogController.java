a46efc555e4828262d855cb48e02baf0b6a9b293
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-11-10 21:26:43 +0100

    Add files via upload

--- 202ToDoListDialogController.java
+++ 202ToDoListDialogController.java
@@ -1,4 +1,29 @@
 package com.gregpaterek.todolist;
 
+import com.gregpaterek.todolist.datamodel.TodoData;
+import com.gregpaterek.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+
+import java.awt.*;
+import java.time.LocalDate;
+
 public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public void processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadLineValue = deadlinePicker.getValue();
+
+        TodoData.getInstance().addTodoItem(new TodoItem(shortDescription, details, deadLineValue));
+    }
 }
