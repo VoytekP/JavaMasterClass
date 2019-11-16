eae9ac10586b76ff434f72ea7525486dcc68d407
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-11-16 06:42:51 +0100

    Add files via upload

--- 2013JavaFXAppController.java
+++ 2013JavaFXAppController.java
@@ -6,10 +6,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
 
 public class Controller {
 
@@ -21,6 +29,9 @@ public class Controller {
 
     @FXML
     private GridPane gridPane;
+
+    @FXML
+    private WebView webView;
 
     public void initialize() {
         button4.setEffect(new DropShadow());
@@ -41,17 +52,35 @@ public class Controller {
     @FXML
     public void handleClick() {
         FileChooser chooser = new FileChooser();
-        chooser.setTitle("Save Application File");
+//        chooser.setTitle("Save Application File");
         chooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Text", "*.txt"),
-                new FileChooser.ExtensionFilter("PDF", "*.pdf")
+                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
         );
 //        DirectoryChooser chooser = new DirectoryChooser();
-        File file = chooser.showSaveDialog(gridPane.getScene().getWindow());
+        List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
         if (file != null) {
-            System.out.println(file.getPath());
+            for (int i=0; i<file.size(); i++) {
+                System.out.println(file.get(i));
+            }
+//            System.out.println(file.getPath());
         } else {
             System.out.println("Chooser was cancelled");
         }
+    }
+
+    @FXML
+    public void handleLinkClick() {
+        try {
+            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+//        WebEngine engine = webView.getEngine();
+//        engine.load("http://wwww.javafx.com");
     }
 }
