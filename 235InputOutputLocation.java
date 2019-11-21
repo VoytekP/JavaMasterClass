6b4bc5ec3d77407f25963ba5563588224e459685
Greg Paterek <51517739+VoytekP@users.noreply.github.com>
2019-11-21 07:00:33 +0100

    Add files via upload

--- 235InputOutputLocation.java
+++ 235InputOutputLocation.java
@@ -1,6 +1,7 @@
 package academy.learnprogramming;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Location {
@@ -12,9 +13,9 @@ public class Location {
         this.locationID = locationID;
         this.description = description;
         if(exits != null) {
-            this.exits = new HashMap<String, Integer>(exits);
+            this.exits = new LinkedHashMap<String, Integer>(exits);
         } else {
-            this.exits = new HashMap<String, Integer>();
+            this.exits = new LinkedHashMap<String, Integer>();
         }
         this.exits.put("Q", 0);
     }
@@ -32,7 +33,7 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<String, Integer>(exits);
+        return new LinkedHashMap<String, Integer>(exits);
     }
     protected void addExit(String direction, int location) {
         exits.put(direction, location);
