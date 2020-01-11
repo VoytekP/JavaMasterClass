7d939effc4457623d25887006883fd3bb821305d
Greg <51517739+VoytekP@users.noreply.github.com>
2020-01-11 14:19:58 +0100

    Add files via upload

---
+++ 20200111TimCharAndBoolean.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightCar = '\u00A9';
+        System.out.println(myCopyrightCar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
