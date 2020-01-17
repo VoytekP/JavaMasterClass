3cbe079220bb94043a2345a2d178b55112917e57
Greg <51517739+VoytekP@users.noreply.github.com>
2020-01-17 22:07:30 +0100

    Add files via upload

---
+++ 20200117TimMegaBytesConverter.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int remainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB" );
+        }
+    }
+
+}
