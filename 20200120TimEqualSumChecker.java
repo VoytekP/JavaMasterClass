46a20b697b531ccc41bdb9a7ce7cb6c5b7731c2c
Greg <51517739+VoytekP@users.noreply.github.com>
2020-01-20 22:04:15 +0100

    Add files via upload

---
+++ 20200120TimEqualSumChecker.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        return (num1 + num2) == num3;
+    }
+
+}
