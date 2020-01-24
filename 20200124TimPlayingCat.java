f8e2e50349387b036e6d5388678b5ab9d7da9e6f
Greg <51517739+VoytekP@users.noreply.github.com>
2020-01-24 09:34:35 +0100

    Add files via upload

---
+++ 20200124TimPlayingCat.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if ((!summer) && (temperature >= 25 && temperature <= 35 )) {
+            return true;
+        }
+        if ((summer) && (temperature >= 25 && temperature <= 45 )) {
+            return true;
+        }
+        return false;
+    }
+}
