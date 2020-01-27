e77b27045ae1e6033124e53109745eccf337659c
Greg <51517739+VoytekP@users.noreply.github.com>
2020-01-27 23:53:14 +0100

    Add files via upload

--- 20200126TimNumberOfDaysInMonth.java
+++ 20200126TimNumberOfDaysInMonth.java
@@ -3,46 +3,47 @@ package academy.learnprogramming;
 public class NumberOfDaysInMonth {
 
     public static boolean isLeapYear(int year) {
-        if ((year < 1 || year > 9999) || (year % 100) != 0){
+        if (year < 1 || year > 9999) {
             return false;
-        } else return ((year % 4) == 0) || ((year % 400) == 0) ;
+        }if ((year % 4) != 0) {
+            return false;
+        }else if ((year % 100) != 0) {
+            return true;
+        } else if ((year % 400) != 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public static int getDaysInMonth(int month, int year) {
         if ((month < 1 || month > 12) || ((year < 1) || year > 9999)) {
             return -1;
         }
-        if (isLeapYear(year)) {
+        else if (isLeapYear(year)) {
             switch (month){
                 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    System.out.println("31");
-                    break;
+                    return 31;
                 case 2:
-                    System.out.println("29");
-                    break;
+                    return 29;
                 case 4: case 6: case 9: case 11:
-                    System.out.println("30");
-                    break;
+                    return 30;
+
                 default:
-                    System.out.println("Invalid date");
-                    break;
+                    return -1;
+
             }
         } else {
             switch (month){
                 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    System.out.println("31");
-                    break;
+                    return 31;
                 case 2:
-                    System.out.println("28");
-                    break;
+                    return 28;
                 case 4: case 6: case 9: case 11:
-                    System.out.println("30");
-                    break;
+                    return 30;
                 default:
-                    System.out.println("Invalid date");
-                    break;
+                    return -1;
             }
         }
-        return month;
     }
 }
