0e11971643fd05bc7d1a332be002b056857feb22
Greg <51517739+VoytekP@users.noreply.github.com>
2020-02-04 23:03:11 +0100

    Add files via upload

---
+++ 20200204AllFactorEx.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class FactorPrinter {
+
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+        for (int i = 1; i <= number; i++)
+            if (number % i == 0)
+                System.out.print(i + " ");
+    }
+}
