3625aee439a94a53dfc1447411f1111346a82358
Greg <51517739+VoytekP@users.noreply.github.com>
2020-02-07 21:44:31 +0100

    Add files via upload

--- 20200206FlourPackerEx.java
+++ 20200206FlourPackerEx.java
@@ -1,20 +1,19 @@
-    public static boolean canPack(int bigCount, int smallCount, int goal) {
-        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+package academy.learnprogramming;
+
+public class FlourPacker {
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) { // (2, 1, 5)
+        if (bigCount < 0 || smallCount < 0 || goal < 0) { // non of the eateries can be negative
             return false;
         }
-
-        bigCount = bigCount * 5;
-
-        if ((goal >= bigCount + smallCount) || ((bigCount == goal) || (bigCount == 0) || smallCount >= goal)) {
-            return true;
-
-        }
-        if (bigCount > goal) {
+        bigCount = bigCount * 5; // all big bags are equal 5 kg
+        int sum = bigCount + smallCount; // the sum of both 5 kg & 1 kg bags
+        if (sum < goal) { // if sum is less then goal its always false
             return false;
         }
-
-        if (bigCount < goal && ((bigCount + smallCount) > goal)) {
-            return true;
+        if (bigCount > goal) { // If the sum is greater than goal, ensure that only full bags are used towards the goal amount.
+            return (((goal / 5) * 5) + smallCount) >= goal; // this equation was the hardest part of the problem
         }
-        return false;
+        return true;
     }
+}
